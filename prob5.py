3249c896a21bf9bfdb5212b501258e3cc091ed9e
Man Mohan Sharma <manmohansharma987@gmail.com>
2014-01-23 23:10:51 +0530

    Update prob5.py
    this idea taken from varun tahnks

--- prob5.py
+++ prob5.py
@@ -10,3 +10,34 @@ def smallest():
 	 	if flag:
 			print i
 			break
+		
+		
+		
+		
+	
+'''this is best way to short out this problem'''
+	
+
+def gcd(a,b):
+	if a<0 or b<0:
+		return -1
+	elif a>b:
+		a,b=b,a
+		
+	while a:
+		p=a
+		a=b%a
+		b=p
+	return b
+
+def fun1():
+	no=1
+	for i in range(1,20):
+		no=(no*i)/gcd(i,no)
+	print no
+
+
+
+
+if __name__=="__main__":
+	fun1()
