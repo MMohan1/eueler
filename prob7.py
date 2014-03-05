ff74333eda3f7da87332e8fc0c796a9a6f081177
manmohan <manmohansharma987@gmail.com>
2014-03-05 18:39:23 +0530

    This is quit better

--- prob7.py
+++ prob7.py
@@ -1,15 +1,18 @@
+def is_prime(i):
+	flag=1
+	for j in range(2,(int(i**0.5)+1)):
+		if i%j==0:
+			flag=0
+			return False
+	if flag:
+		return True
 def p10001n0():
-	count=0
-	sum1=0
-	for i in range(2,2000000):
-		flag=1
-		for j in range(2,(int(i**0.5)+1)):
-			if i%j==0:
-				flag=0
-				break
-		if flag:
-			count+=1
-			print count,'==>',i
-			sum1+=i
-	print sum1
-			
+	i = 2
+	count = 0
+	while count != 600:
+			if is_prime(i):
+				count += 1
+			i += 1
+	print i-1
+if __name__ == '__main__':
+	p10001n0()			
