c4a1ba0425c0155295b1e3ba75e1d5080de3f134
manmohan <manmohansharma987@gmail.com>
2014-03-19 04:40:22 +0530

    This is fr 76 but it is taking more time

---
+++ prob76.py
@@ -0,0 +1,16 @@
+import math
+def function(num=5,minimal=1):
+	temp = 1
+	if num <= 1:
+		return 1
+	else:
+		for i in range(1, int(math.floor(num/2))+1):
+			if i >= minimal:
+				temp += function(num-i, i)
+	return temp
+
+def main():
+	print function(100,1)-1
+
+if __name__ == "__main__":
+	main()
