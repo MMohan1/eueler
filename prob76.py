e5d85f3066921715f2864279dd3b6371a9461472
manmohan <manmohansharma987@gmail.com>
2014-03-19 07:51:50 +0530

    s is more correct and fast way.....but very forward

--- prob76.py
+++ prob76.py
@@ -1,4 +1,4 @@
-import math
+'''import math
 
 def function(num=5,minimal=1):
 	temp = 1
@@ -11,7 +11,27 @@ def function(num=5,minimal=1):
 	return temp
 
 def main():
-	print function(100,1)-1
+	print function(100,1)-1'''
+
+
+
+def prob76():
+	target = 10
+	ns = range(1,10)
+	ways = [1] +[0]*target
+	print ways
+	#print ns
+	for n in ns:
+		for i in range(n,target+1):
+			#print ways
+			ways[i] += ways[i-n]
+			#import pdb;pdb.set_trace()
+	print ways	
+	print ways[target]
+
+
+if __name__ == '__main__':
+	prob76()
 
 if __name__ == "__main__":
 	main()
